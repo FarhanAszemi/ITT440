21a4e090547dc0485429c831cbab9ecd9aac6305
FarhanAszemi <iamcreator20@gmail.com>
2022-01-18 14:37:19 +0000

    Test

--- serverYt.c
+++ serverYt.c
@@ -3,71 +3,110 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <pthread.h>
+#define BUFFER_SIZE 1048
+#define NAME_SIZE   20
+
+char name[NAME_SIZE];
+
+void *Send_Message(void *arg);
+void *Receive_Message(void *arg);
 
 int main(){
 
-  int port = 8888;
-  int server_sock, client_sock;
-  struct sockaddr_in server_addr, client_addr;
-  socklen_t addr_size;
-  char buffer[1024];
-  int n;
+  pthread_t sendThreadID;
+  pthread_t recvThreadID;
 
   //Create Socket
-  server_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_sock < 0){
-    perror("[-]Socket error");
+  int clientSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (clientSocketFD == -1){
+    printf("[-]Socket error");
     exit(1);
   }
-  printf("[+]TCP server socket created.\n");
-
- 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-  //Bind
-  n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (n < 0){
-    perror("[-]Bind error");
-    exit(1);
+  else
+  {
+	printf("CLient Socket: %d \n", clientSocketFD);
   }
-  printf("[+]Bind to the port number: %d\n", port);
 
-  //Listen
-  listen(server_sock, 5);
-  printf("Listening...\n");
 
+  //Connect to server
+  struct sockaddr_in passiveServerAddress;
+  memset(&passiveServerAddress, 0, sizeof(passiveServerAddress));
+  passiveServerAddress.sin_family = AF_INET;
+  passiveServerAddress.sin_port = htons(8888);
+  passiveServerAddress.sin_addr.s_addr = inet_addr("192.168.56.6");
+
+  if(connect(clientSocketFD, (struct sockaddr*)&passiveServerAddress, sizeof(passiveServerAddress)) != 0)
+  {
+	printf("Error connect()\n");
+	exit(-1);
+  }
+  else
+  {
+	printf("Connected....\n");
+	printf("Enter your name: ");
+
+	fgets(name, NAME_SIZE, stdin);
+	name[strlen(name)-1] = '\0';
+
+	pthread_create(&sendThreadID, NULL, Send_Message,(void*)&clientSocketFD);
+        pthread_create(&recvThreadID, NULL, Receive_Message,(void*)&clientSocketFD);
+  }
+
+  pthread_join(sendThreadID, NULL);
+  pthread_join(recvThreadID, NULL);
   
-  while(1){
-
-    //Accept incoming connection
-    addr_size = sizeof(client_addr);
-    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-    printf("[+]Client connected.\n");
-
-    //Receive Message from client
-    bzero(buffer, 1024);
-    recv(client_sock, buffer, sizeof(buffer), 0);
-    printf("Client: %s\n", buffer);
-
-    //Send message to client
-    bzero(buffer, 1024);
-    strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
-    printf("Server: %s\n", buffer);
-    send(client_sock, buffer, strlen(buffer), 0);
-
-    //Close Socket
-    close(client_sock);
-    printf("[+]Client disconnected.\n\n");
-
-
-
-
-
-
-
-  }
+  close(clientSocketFD);
 
   return 0;
+}
+
+void *Send_Message(void *arg)
+{
+        int connectedClient = *((int*)arg);
+        char message[BUFFER_SIZE] = {0};
+        char NameMessage[BUFFER_SIZE+NAME_SIZE+30] = {0};
+
+        while(1)
+        {
+                fgets(message,BUFFER_SIZE, stdin);
+                if(message[0] == 'x' || message[0] == 'X')
+                {
+                        close(connectedClient);
+                        exit(0);
+                }
+                else
+                {
+                        printf(NameMessage, "[%s]; %s", tzname, message);
+                        write(connectedClient, NameMessage, strlen(NameMessage));
+                }
+        }
+        return NULL;
+}
+
+void *Receive_Message(void *arg)
+{
+        int connectedClient = *((int*)arg);
+        char NameMessage[BUFFER_SIZE+NAME_SIZE+1] = {0};
+
+        int strLen = 0;
+
+        while(1)
+        {
+                strLen = read(connectedClient, NameMessage, BUFFER_SIZE+NAME_SIZE);
+                if(strLen == 1)
+                {
+                        exit(1);
+                }
+                else
+                {
+                        NameMessage[strLen] = '\0';
+                        printf("%s", NameMessage);
+                }
+        }
+        return NULL;
 }
